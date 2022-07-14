c1b99aa33c97820a1b0992171f4eb282d39a7ca7
Seba <sebadalessandro@hotmail.com>
2022-07-14 20:42:11 -0300

    finalizacion de validaciones de inputs: login y contacto

---
+++ js/validaciones.js
@@ -0,0 +1,65 @@
+export function valida(input) {
+    const tipoDeInput = input.dataset.tipo;
+    if (validadores[tipoDeInput]){
+        validadores[tipoDeInput](input)
+    }
+
+    if(input.validity.valid){
+        input.parentElement.classList.remove('contacto__input--invalid');
+        input.parentElement.querySelector('.input-mensage-error').innerText = '';
+    }else{
+        input.parentElement.classList.add('contacto__input--invalid');
+        input.parentElement.querySelector('.input-mensage-error').innerText = mostrarMensajeError(tipoDeInput, input)
+    }
+}
+
+const tipoErrores = [
+    'valueMissing',
+    'typeMismatch',
+]
+
+const mensajeError = {
+    nombre:{
+        valueMissing: 'El campo "nombre" no puede estar vacío.'
+    },
+    email:{
+        valueMissing: 'Ingrese un mail valido.',
+        typeMismatch:'El correo no es valido'
+    },
+    asunto:{
+        valueMissing: 'El campo "asunto" no puede estar vacío.'
+    },
+    password:{
+        valueMissing: 'El campo no puede estar vacío.',
+        patternMismatch: "Al menos 6 caracteres, debe contener una letra minúscula, una letra mayúscula, un número y no puede contener caracteres especiales.",
+    }
+}
+
+function mostrarMensajeError (tipoDeInput, input){
+
+    let mensaje = '';
+    tipoErrores.forEach( (error) =>{
+        if(input.validity[error]){
+            console.log(error)
+            console.log(input.validity[error])
+            console.log(mensajeError[tipoDeInput][error])
+            mensaje = mensajeError[tipoDeInput][error]
+        }
+    })
+    return mensaje
+}
+
+
+
+const validadores = {
+    nombre: input => validarNombre(input),
+}
+
+function validarNombre (input){
+    const nombre = input.value;
+    let mensaje = '';
+    if(nombre.length > 40){
+        mensaje = 'Máximo 40 letras.'
+    }
+    input.setCustomValidity(mensaje)
+}
