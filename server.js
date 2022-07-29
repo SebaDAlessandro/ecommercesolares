bd9cac7a7622de3fe9b17e9deab1f24458e647f0
Seba <sebadalessandro@hotmail.com>
2022-07-29 15:57:41 -0300

    cargando en github todo el contenido

--- server.js
+++ server.js
@@ -7,11 +7,7 @@ const middlewares = jsonServer.defaults()
 server.use(middlewares)
 server.use(router)
 
-const url = 'https://json-server-ecommercelamp.herokuapp.com/'
-
-//process.env.PORT 
-
-const port = url || 3000
+const port =  process.env.PORT  || 3000
 
 server.listen(port, () => {
     console.log(`JSON Server is running on port ${port}`)
