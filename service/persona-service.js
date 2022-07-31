10f5f027398f63db8858d6e73ffce39adb4f827a
Seba <sebadalessandro@hotmail.com>
2022-07-30 23:26:59 -0300

    modificando persona-service.js con el directorio de Heroku

--- service/persona-service.js
+++ service/persona-service.js
@@ -1,7 +1,7 @@
 const urlPersonas = 'https://ecommersolares.herokuapp.com/perfil'
 const localPersonas = 'http://localhost:3000/perfil'
 
-const listaPersonas = ()=> fetch(localPersonas).then((personas)=> personas.json())
+const listaPersonas = ()=> fetch(urlPersonas).then((personas)=> personas.json())
 
 export const personaServices = {
     listaPersonas,
